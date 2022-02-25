6ec3952be7257046543ee9cb3bcbcb38206a6ba9
devtest23 <devtest7281@gmail.com>
2022-02-25 10:33:13 +0530

    specs added

--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../Services/api.service';
 
 import { DemoComponent } from './demo.component';
 
@@ -8,9 +11,10 @@ describe('DemoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DemoComponent ]
-    })
-    .compileComponents();
+      declarations: [DemoComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [ApiService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/url-shortner/url-shortner.component.spec.ts
+++ src/app/url-shortner/url-shortner.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../Services/api.service';
 
 import { UrlShortnerComponent } from './url-shortner.component';
 
@@ -8,9 +15,17 @@ describe('UrlShortnerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UrlShortnerComponent ]
-    })
-    .compileComponents();
+      declarations: [UrlShortnerComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        FormsModule,
+      ],
+      providers: [ApiService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
